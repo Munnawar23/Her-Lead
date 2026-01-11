b5f2fe62ca07b30cefe00b39aa48d30a1d49522b
Munnawar23 <munawwarh48@gmail.com>
2026-01-12 00:05:55 +0530

    setup fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useRef } from 'react'
+import { useRef } from 'react'
 import { useRouter } from 'next/navigation'
 import { gsap } from 'gsap'
 import { useGSAP } from '@gsap/react'
@@ -10,7 +10,6 @@ const SplashScreen = () => {
   const containerRef = useRef<HTMLDivElement>(null)
   const line1Ref = useRef<HTMLHeadingElement>(null)
   const line2Ref = useRef<HTMLParagraphElement>(null)
-  const line3Ref = useRef<HTMLParagraphElement>(null)
 
   useGSAP(() => {
     const timeline = gsap.timeline()
@@ -44,20 +43,7 @@ const SplashScreen = () => {
         },
         '-=0.4' // Start slightly before previous animation ends
       )
-      .fromTo(
-        line3Ref.current,
-        { 
-          opacity: 0, 
-          x: 100 
-        },
-        { 
-          opacity: 1, 
-          x: 0, 
-          duration: 1.2,
-          ease: 'power3.out'
-        },
-        '-=0.4'
-      )
+
       // Wait for 1 second after all text animations
       .to({}, { duration: 1 })
       // Slide the entire page up
@@ -92,12 +78,7 @@ const SplashScreen = () => {
         >
           A 360° Digital Elevation Agency
         </p>
-        <p 
-          ref={line3Ref}
-          className='text-lg sm:text-2xl md:text-3xl text-white opacity-0'
-        >
-          Creating Impactful & Measurable Results
-        </p>
+
       </div>
     </div>
   )
